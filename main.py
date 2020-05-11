37fb348fb95f00e3e4c29c09833635fba1e23f86
Phate334 <phate334@gmail.com>
2020-05-11 22:27:32 +0800

    [add] convert to json.

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+import json
+from datetime import datetime
+
 import pdfplumber
 
-with pdfplumber.open("2020.pdf") as pdf, open('out.txt', 'w', encoding='utf-8') as f:
+out = {
+    'name': '高雄市第3屆市長罷免案投開票所設置地點一覽表',
+    'datetime': '1589205495',
+    'header': ['投開票所編號', '投開票所名稱', '投開票所地址', '所屬村里', '所屬鄰別', '備註'],
+    'data': []
+}
+
+parse_time = int(datetime.now().timestamp())
+
+with pdfplumber.open('090511.pdf') as pdf, open('{}.json'.format(parse_time), 'w', encoding='utf-8') as f:
     for page in pdf.pages:
-        for row in page.extract_table():
-            f.write('|'.join([r.replace('\n', '') for r in row]) + '\n')
+        rows = page.extract_table()
+        header = rows.pop(0)
+        for row in rows:
+            out['data'].append([r.replace('\n', '') for r in row])
+    f.write(json.dumps(out, ensure_ascii=False))
